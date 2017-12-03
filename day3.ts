943de132a0461c53221e55bb1c35c6b15ffde955
Fame <SlavaNek@yandex.ru>
2017-12-03 20:12:29 +0200

    day3

--- day3.ts
+++ day3.ts
@@ -8,38 +8,111 @@ run().then(([result1, result2]) => {
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input) {
-  let result = 0;
+function calculatePart1(input: number): number {
+  const pos = getPos(input);
+  return Math.abs(pos[0]) + Math.abs(pos[1]);
+}
+
+function calculatePart1_2(input: number): number {
+  const rowSize = Math.ceil(Math.sqrt(input)) & -2;
+  const maxValueInCircle = Math.pow(rowSize + 1, 2);
+  return rowSize - (((maxValueInCircle - input) % rowSize) || 0);
+}
+
+
+function calculatePart2(input: number): number {
+  let result = 1;
+  let i = 2;
+  const map = {'0,0': 1};
+  while (result <= input) {
+    const pos = getPos(i);
+    result = 0;
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        if (x || y)
+          result+= (+map[posToString([pos[0]+x, pos[1]+y])]) || 0
+      }
+    }
+    map[posToString(pos)] = result;
+    i++
+  }
 
   return result;
 }
 
-function calculatePart2(input) {
-  let result = 0;
-
-  return result;
+function posToString(pos: [number, number]): string {
+  return pos[0].toString() + ',' + pos[1].toString();
 }
 
-
-function parse(input: string) {
-  const regexp = / /g;
-  return input.split('\n')
-    .map(row => row.match(regexp))
+function getPos(input: number): [number, number] {
+  const map = [[1, 1], [-1, 1], [-1,-1], [1, -1]];
+  const rowSize = Math.ceil(Math.sqrt(input)) & -2;
+  const pos = rowSize / 2;
+  const maxValueInCircle = Math.pow(rowSize + 1, 2);
+  const side = Math.floor((maxValueInCircle - input) / rowSize) || 0;
+  const diff = ((maxValueInCircle - input) % rowSize) || 0;
+  return (side % 2) ? [pos * map[side][0], (pos - diff) * map[side][1]] :
+                      [(pos - diff) * map[side][0], pos * map[side][1]];
 }
+
+function getPos_2(input: number): [number, number] {
+  let steps = 0;
+  let index = 1;
+  let x = 0;
+  let y = 0;
+  while (true) {
+    if (index === input)
+      return [x, y];
+    steps++;
+    for (let i = 0; i < steps; i++) {
+      index++;
+      x++;
+      if (index === input)
+        return [x, y];
+    }
+    for (let i = 0; i < steps; i++) {
+      index++;
+      y--;
+      if (index === input)
+        return [x, y];
+    }
+    steps++;
+    for (let i = 0; i < steps; i++) {
+      index++;
+      x--;
+      if (index === input)
+        return [x, y];
+    }
+    for (let i = 0; i < steps; i++) {
+      index++;
+      y++;
+      if (index === input)
+        return [x, y];
+    }
+  }
+}
+
 
 async function run() {
-  const input = await getInput(DAY);
-  const parsed = parse(input);
-  return [calculatePart1(parsed), calculatePart2(parsed)]
+  const input = +(await getInput(DAY));
+  return [calculatePart1(input), calculatePart2(input)]
 }
 
 function test() {
-  const test1 = ``;
-  const testPart1 = getTestFunctin(input => calculatePart1(parse(input)));
-  testPart1(test1, 0);
+  const testPart1 = getTestFunctin(calculatePart1);
+  testPart1(1, 0);
+  testPart1(12, 3);
+  testPart1(23, 2);
+  testPart1(1024, 31);
 
-  const test2 = ``;
-  const testPart2 = getTestFunctin(input => calculatePart2(parse(input)));
-  testPart2(test2, 0);
 
+  const testPart2 = getTestFunctin(calculatePart2);
+  testPart2(1, 2);
+  testPart2(2, 4);
+  testPart2(4, 5);
+  testPart2(7, 10);
+  testPart2(10, 11);
+  testPart2(16, 23);
+  testPart2(100, 122);
+  testPart2(351, 362);
 }
--- helper.ts
+++ helper.ts
@@ -3,6 +3,8 @@ const http = require('http');
 
 const session = '53616c7465645f5ff606a55b6e6a43c5ed75d0a3a2e357ee6caa1f99231c79436bc89a8bcb7b24b62ff34df27bd8c8ae';
 
+type strOrNum = string | number;
+
 export function getInput(day: number): Promise<string> {
   return new Promise((resolve, reject) => {
     const options = {
@@ -31,8 +33,8 @@ export function getInput(day: number): Promise<string> {
   })
 }
 
-export function getTestFunctin(call: (input) => string | number): (data: string, expected: string | number) => boolean {
-  return (data: string, expected: string | number) => {
+export function getTestFunctin(call: (input) => strOrNum): (data: strOrNum, expected: strOrNum) => boolean {
+  return (data: string, expected: strOrNum) => {
     const actual = call(data);
     if (actual == expected)
       console.log('\x1b[32mSuccess test\x1b[0m:\x1b[36m', data,'\x1b[32m', actual, '\x1b[0m');
