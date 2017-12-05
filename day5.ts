9b6e0f7eba3bde63c38cdadb0651e874de96cc91
Fame <SlavaNek@yandex.ru>
2017-12-05 07:58:46 +0200

    day5

--- day5.ts
+++ day5.ts
@@ -8,38 +8,48 @@ run().then(([result1, result2]) => {
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input) {
+function calculatePart1(input: number[]): number {
   let result = 0;
+  let cursor = 0;
+  while ((cursor >= 0) && (cursor < input.length)) {
+    cursor += input[cursor]++;
+    result++;
+  }
 
   return result;
 }
 
 function calculatePart2(input) {
   let result = 0;
+  let cursor = 0;
+  while ((cursor >= 0) && (cursor < input.length)) {
+    cursor += input[cursor] >= 3 ? input[cursor]-- : input[cursor]++;
+    result++;
+  }
 
   return result;
 }
 
 
-function parse(input: string) {
-  const regexp = /\S+/g;
+function parse(input: string): number[] {
+  const regexp = /\n+/g;
   return input.split('\n')
-    .map(row => row.match(regexp))
+    .map(item => +item)
 }
 
 async function run() {
   const input = await getInput(DAY);
-  const parsed = parse(input);
-  return [calculatePart1(parsed), calculatePart2(parsed)]
+  const parsed1 = parse(input);
+  const parsed2 = parse(input);
+  return [calculatePart1(parsed1), calculatePart2(parsed2)]
 }
 
 function test() {
-  const test1 = ``;
-  const testPart1 = getTestFunction(input => calculatePart1(parse(input)));
-  testPart1(test1, 0);
+  const test = `0 3 0 1 -3`;
+  const testPart1 = getTestFunction(input => calculatePart1(input.split(' ').map(v => +v)));
+  testPart1(test, 5);
 
-  const test2 = ``;
-  const testPart2 = getTestFunction(input => calculatePart2(parse(input)));
-  testPart2(test2, 0);
+  const testPart2 = getTestFunction(input => calculatePart2(input.split(' ').map(v => +v)));
+  testPart2(test, 10);
 
 }
