0071076ce8607044e68040c06e5aa3bad6f01cdf
Fame <SlavaNek@yandex.ru>
2017-12-06 20:28:18 +0200

    day6

--- day6.ts
+++ day6.ts
@@ -1,45 +1,70 @@
 import {getInput, getTestFunction} from './helper';
-
 const DAY = 6;
-
 test();
 run().then(([result1, result2]) => {
   console.log('Part 1:', result1);
   console.log('Part 2:', result2);
 });
-
-function calculatePart1(input) {
+function calculatePart1(input: number[]) {
   let result = 0;
-
-  return result;
+  const map = {[arrToString(input)]: true};
+  while (true) {
+    result++;
+    rebase(input);
+    if (map[arrToString(input)]) {
+      return result;
+    }
+    map[arrToString(input)] = true;
+  }
 }
-
-function calculatePart2(input) {
+function calculatePart2(input: number[]) {
   let result = 0;
-
-  return result;
+  const map = {[arrToString(input)]: 0};
+  while (true) {
+    result++;
+    rebase(input);
+    if (map[arrToString(input)]) {
+      return result - map[arrToString(input)];
+    }
+    map[arrToString(input)] = result;
+  }
 }
-
-
-function parse(input: string) {
-  const regexp = /\S+/g;
-  return input.split('\n')
-    .map(row => row.match(regexp))
+function arrToString(arr: number[]): string {
+  return arr.join('_');
 }
-
+function getMaxIndex(arr: number[]) {
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > arr[max]) {
+      max = i;
+    }
+  }
+  return max;
+}
+function rebase(arr: number[]) {
+  let maxIndex = getMaxIndex(arr);
+  const size = arr.length;
+  let buff = arr[maxIndex];
+  arr[maxIndex] = 0;
+  while (buff > 0) {
+    arr[++maxIndex % size]++;
+    buff--;
+  }
+  return arr;
+}
+function parse(input: string): number[] {
+  return input.split(/\s+/)
+    .map(item => +item)
+}
 async function run() {
   const input = await getInput(DAY);
   const parsed = parse(input);
   return [calculatePart1(parsed), calculatePart2(parsed)]
 }
-
 function test() {
-  const test1 = ``;
+  const test = `0 2 7 0`;
   const testPart1 = getTestFunction(input => calculatePart1(parse(input)));
-  testPart1(test1, 0);
-
-  const test2 = ``;
+  testPart1(test, 5);
   const testPart2 = getTestFunction(input => calculatePart2(parse(input)));
-  testPart2(test2, 0);
-
+  testPart2(test, 4);
 }
