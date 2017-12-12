16e9a2d0f5846409d7b210efd8877d1cf0434ab2
Fame <SlavaNek@yandex.ru>
2017-12-12 08:23:36 +0200

    day12

--- day12.ts
+++ day12.ts
@@ -2,6 +2,8 @@ import {getInput, getTestFunction} from './helper';
 
 const DAY = 12;
 
+let map: Pipe[] = [];
+
 test();
 run().then(([result1, result2]) => {
   console.log('Part 1:', result1);
@@ -9,38 +11,82 @@ run().then(([result1, result2]) => {
 });
 
 function calculatePart1(input) {
-  let result = 0;
-
-  return result;
+  map = input;
+  const set = getGroup(0);
+  map = [];
+  return set.size;
 }
 
 function calculatePart2(input) {
-  let result = 0;
+  map = input;
+  const groupSet: Set<number> = new Set()
+  for (let i = 0; i < map.length; i++) {
+    if (map[i].group === undefined) {
+      getGroup(i);
+      groupSet.add(i);
+    }
+  }
+  map = [];
+  return groupSet.size;
+}
 
-  return result;
+function getGroup(start: number): Set<number> {
+  const set: Set<number> = new Set();
+  const queue = [];
+  queue.push(start);
+  set.add(start);
+  while (queue.length > 0) {
+    const index = queue.shift();
+    map[index].group = start;
+    for (let prog of map[index].connections) {
+      if (!set.has(prog)) {
+        set.add(prog);
+        queue.push(prog);
+      }
+    }
+  }
+  return set;
 }
 
 
-function parse(input: string) {
-  const regexp = /\S+/g;
-  return input.split('\n')
-    .map(row => row.match(regexp))
+function parse(input: string): Pipe[] {
+  let map: Pipe[] = [];
+  const regexp = /(\d+)\s<->\s(.*)/;
+  input.split('\n')
+    .map(row => regexp.exec(row))
+    .map(res => {
+      map[+res[1]] = {connections: res[2].split(', ').map(v => +v.trim())}
+    });
+  return map;
 }
 
 async function run() {
   const input = await getInput(DAY);
-  const parsed = parse(input);
-  return [calculatePart1(parsed), calculatePart2(parsed)]
+  const parsed1 = parse(input);
+  const parsed2 = parse(input);
+  return [calculatePart1(parsed1), calculatePart2(parsed2)]
 }
 
 function test() {
+  const test = `0 <-> 2
+1 <-> 1
+2 <-> 0, 3, 4
+3 <-> 2, 4
+4 <-> 2, 3, 6
+5 <-> 6
+6 <-> 4, 5`;
   const testPart1 = getTestFunction(input => calculatePart1(parse(input)));
-  testPart1(0, 0);
+  testPart1(test, 6);
+  map = [];
   console.log('---------------------');
 
   const testPart2 = getTestFunction(input => calculatePart2(parse(input)));
-  testPart2(0, 0);
+  testPart2(test, 2);
+  map = [];
   console.log('---------------------');
+}
 
-
+interface Pipe {
+  connections: number[];
+  group?: number;
 }
