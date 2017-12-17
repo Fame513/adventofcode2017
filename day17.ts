792f5cafebace50641e7208489cb514923d4708a
Fame <SlavaNek@yandex.ru>
2017-12-17 07:59:25 +0200

    day17

--- day17.ts
+++ day17.ts
@@ -8,37 +8,39 @@ run().then(([result1, result2]) => {
   console.log('Part 2:', result2);
 });
 
-function calculatePart1(input) {
-  let result = 0;
+function calculatePart1(input: number, year: number): number {
+  const buf = [0];
+  let pos = 0;
 
-  return result;
+  for (let i = 1; i < year; i++) {
+    pos = ((pos + input) % buf.length) + 1;
+    buf.splice(pos, 0, i);
+  }
+  return buf[pos + 1]
 }
 
-function calculatePart2(input) {
+function calculatePart2(input: number, year: number): number {
   let result = 0;
-
+  let pos = 0;
+  for (let i = 1; i < year; i++) {
+    pos = ((pos + input) % i) + 1;
+    if (pos === 1)
+      result = i;
+  }
   return result;
-}
-
-function parse(input: string) {;
-  const regexp = /\d+/;
-  return input.split('\n')
-    .map(row => regexp.exec(row));
 }
 
 async function run() {
   const input = await getInput(DAY);
-  const parsed = parse(input);
-  return [calculatePart1(parsed), calculatePart2(parsed)]
+  return [calculatePart1(+input, 2018), calculatePart2(+input, 50000001)]
 }
 
 function test() {
-  const test1 = ``;
-  const testPart1 = getTestFunction(input => calculatePart1(parse(input)));
-  testPart1(test1, 0);
+  const testPart1 = getTestFunction(input => calculatePart1(input[0], input[1]));
+  testPart1([3, 2018], 638);
   console.log('---------------------');
 
-  const testPart2 = getTestFunction(input => calculatePart2(parse(input)));
-  testPart2(test1, 0);
+  const testPart2 = getTestFunction(input => calculatePart2(input[0], input[1]));
+  testPart2([3, 10], 9);
   console.log('---------------------');
 }
