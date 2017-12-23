5f7b8e089b3a215f81b005a393223e2e3e1f1638
Fame <SlavaNek@yandex.ru>
2017-12-23 18:21:11 +0200

    day23(part2)

--- day23.ts
+++ day23.ts
@@ -7,7 +7,6 @@ export class Command {
   data: any[];
 }
 
-test();
 run().then(([result1, result2]) => {
   console.log('Part 1:', result1);
   console.log('Part 2:', result2);
@@ -41,9 +40,7 @@ function calculatePart1(input: Command[]): number {
         break;
       }
     }
-    if (cursor === 23) {
-      console.log(data['g'])
-    }
+    
     cursor++;
   }
   return result;
@@ -117,67 +114,28 @@ function parse(input: string): Command[] {
 }
 
 function prog(): number {
-let a = 0, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
+let a = 1, b = 0, c = 0, d = 0, e = 0, f = 0, g = 0, h = 0;
 
   b = 99;
   c = b;
   if (a !== 0) {
-    console.log('in A');
-    b *= 100;
-    b += 100000;
-    c = b;
-    c += 17000;
+    b = (b * 100) + 100000;
+    c = b + 17000;
   }
-  while (true) {
-    f = 1;
-    d = 2;
-    do {
-      e = 2;
-      do {
-        g = d;
-        g *= e;
-        g -= b;
-
-        if (g === 0) {
-          f = 0;
-        }
-
-        e++;
-        g = e;
-        g -= b;
-      } while (g !== 0);
-
-      d++;
-      g = d;
-      g -= b;
-    } while (g !== 0);
-
-    if (f === 0) {
-      h++
+  for (;b <= c; b += 17) {
+    for (d = 2; d < b; d++) {
+      if (b % d === 0) {
+        h++;
+        break;
+      }
     }
-
-    g = b;
-    g -= c;
-
-    if (g === 0) {
-      return h;
-    }
-    b += 17;
   }
+  return h;
   
 }
 
 async function run() {
   const input = await getInput(DAY);
   const parsed = parse(input);
-  return [calculatePart1(parsed), 0]
-}
-
-function test() {
-  console.log('prog', prog());
-  console.log('---------------------');
-
-  // const testPart2 = getTestFunction(input => calculatePart2(parse(input)));
-  // testPart2(test1, 0);
-  // console.log('---------------------');
+  return [calculatePart1(parsed), prog()]
 }
